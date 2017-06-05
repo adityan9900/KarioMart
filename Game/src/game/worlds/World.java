14c6f9d64f9e9395b53539e877ba05eeff232c32
avellal14 <avellal14@LAPTOP-8UGIUUDM>
2017-06-04 22:49:10 -0400

    just added inner and outer paths plus code to read from csv

--- Game/src/game/worlds/World.java
+++ Game/src/game/worlds/World.java
@@ -2,6 +2,8 @@ package game.worlds;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 import game.gfx.ImageLoader;
@@ -9,7 +11,10 @@ import game.utils.Utils;
 
 public class World {
 	
-	private ArrayList<int[]> path;
+	private ArrayList<int[]> centerPath;
+	private ArrayList<int[]> innerPath;
+	private ArrayList<int[]> outerPath;
+	
 	private BufferedImage bkg;
 	private String trackName;
 	private int trackWidth;
@@ -18,12 +23,41 @@ public class World {
 	private int mapHeight;
 	
 	public World(String name, int width) {
-		path = new ArrayList<int[]>();
+		centerPath = new ArrayList<int[]>();
+		innerPath = new ArrayList<int[]>();
+		outerPath = new ArrayList<int[]>();
+		
 		trackName = name;
 		trackWidth = width;
 		loadWorld();
 	}
 	
+	//i = 0 --> inner, i = 1 --> center, i = 2 --> outer
+	public void setPath(String csv, int i) {
+		try {
+			BufferedReader r = new BufferedReader(new FileReader(csv));
+			String currentLine = "";
+			while(true) {
+				currentLine = r.readLine();
+				if(currentLine == null) break;
+				String [] a = currentLine.split(",");
+				
+				int x = Integer.parseInt(a[0]);
+				int y = Integer.parseInt(a[1]);
+				
+				int [] w = new int[2];
+				w[0] = x;
+				w[1] = y;
+				
+				if(i == 0) innerPath.add(w);
+				else if(i == 1) centerPath.add(w);
+				else outerPath.add(w);
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Failure in processing!");
+		}
+	}
 	public void tick() {
 		
 	}
